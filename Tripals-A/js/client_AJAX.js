1f816f3dfc2307fa05cb426e7541daafea2112b6
saruhiko80155 <saruhiko80155@gmail.com>
2023-04-24 23:31:36 +0800

    大頭照上傳

--- Tripals-A/js/client_AJAX.js
+++ Tripals-A/js/client_AJAX.js
@@ -1,17 +1,16 @@
 $(document).ready(function () {
     function getAvatar() {
         //GET 大頭貼照片
+        let url = 'http://localhost:3000';
         $.ajax({
             type: "GET",
-            url: 'http://localhost:3000/client/avatar',
+            url: url + '/client/avatar',
             success: function (data) {
                 // 取得圖片資源成功，顯示圖片
-                // $(".h-img").empty();
+                $(".h-img").empty();
                 $('.imgPreviewBorder').empty();
-                // avator(data.avatar)
-                console.log("Helllo" + JSON.stringify(data));
-                // $('.shot').attr('src','file://Users/chenxin/Documents/GitHub/Tripals-Backend/router/public/userno_2/avatar.png')
-                console.log('我是5500');
+                avator(url + data.avatar)
+                console.log(data);
             },
             error: function (err) {
                 // 發生錯誤，顯示錯誤訊息
@@ -19,9 +18,9 @@ $(document).ready(function () {
             }
         });
     }
-    function avator(avatars, uerName) {
-        $('.h-img').append(`<img class="shot" src="./userprofile/userno_2${avatars}" alt="shot" />`);
-        $('.imgPreviewBorder').append(`<img class="imgPreview" src='./userprofile/userno_2${avatars}' alt="大頭貼預覽">`);
+    function avator(avatars) {
+        $('.h-img').append(`<img class="shot" src=${avatars} alt="shot" />`);
+        $('.imgPreviewBorder').append(`<img class="imgPreview" src=${avatars} alt="大頭貼預覽">`);
 
     };
     getAvatar();
@@ -49,8 +48,8 @@ $(document).ready(function () {
     let preview = document.querySelector('.imgPreview');
     let inputFile = document.querySelector('#shotUpload');
 
-    inputFile.addEventListener('change', function () {
-        let file = inputFile.files[0];
+    inputFile.addEventListener('change', (e) => {
+        let file = e.target.files[0];
         console.log(file);
         let reader = new FileReader();
         reader.readAsDataURL(file);
@@ -62,26 +61,24 @@ $(document).ready(function () {
                 preview.setAttribute("src", e.target.result);
             }
         }
+        $('.uploadShotDone').on('click', function () {
+            console.log(file);
+            let formData = new FormData();
+            formData.append('shotUpload',file);
+            // console.log(formData.get('shotUpload'));//檢查formData是否真的有東西
+            $.ajax({
+                url: 'http://localhost:3000/client/upload',
+                type: 'POST',
+                data: formData,
+                processData: false,
+                contentType: false,
+                success: function () {
+                    console.log('okkkk');
+                    alert('Upload successful!');
+                },
+            });
+        })
+
     })
 
-    $('.uploadShotDone').on('click', function () {
-        // console.log('??')
-        let file = inputFile.files[0];
-        console.log('第二次'+inputFile);
-        var formData = new FormData();
-        formData.append('avatar', $('#shotUpload').file);
-        console.log(formData);
-        $.ajax({
-            url: 'http://localhost:3000/client/upload',
-            type: 'POST',
-            data: formData,
-            processData: false,
-            contentType: false,
-            success: function () {
-                console.log('okkkk');
-                alert('Upload successful!');
-                // $('#avatarImg').attr('src', 'http://localhost:3000/client/uploadfile' + new Date().getTime());
-            },
-        });
-    })
 });
