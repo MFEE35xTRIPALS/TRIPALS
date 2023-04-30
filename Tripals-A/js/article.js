0ac0942992fd27b11e917d1afb2ed633dedd3ea4
Tina <t2239tina@gmail.com>
2023-04-30 12:47:32 +0800

    旅遊導覽頁css調整

--- Tripals-A/js/article.js
+++ Tripals-A/js/article.js
@@ -3,3 +3,20 @@ new TypeIt("#ml6", {
   strings: "Start your Trip.",
   waitUntilVisible: true,
 }).go();
+/* ------------- CRUD ajax ------------- */
+var url = "http://localhost:3000";
+
+/* ------------- hashtag random 8 ------------- */
+$.ajax({
+  url: url + "/articles/hashtags",
+  type: "GET",
+  success: function (data) {
+    $("#tags").empty();
+    $.each(data, function (i, list) {
+      var tag = `<li class="tag" ># ${list.hashtag}
+      </li>`;
+      $("#tags").append(tag);
+      // console.log(list);
+    });
+  },
+});
