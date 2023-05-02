31ce95d8dfa871105e72f63d0d0a3db05288410b
Tina <t2239tina@gmail.com>
2023-05-02 14:56:00 +0800

    admin news 判斷式

--- Tripals-A/js/admin_ajax.js
+++ Tripals-A/js/admin_ajax.js
@@ -44,7 +44,7 @@ var renderNews = () => {
           newsTr.append(`<td class="a-noUse">${list.userid}</td>`);
           newsTr.append(`<td class="x-icon" >${sta}</td>`);
           newsTr.append(
-            `<td class="icon"><i class="fa-regular fa-pen-to-square"></i></td>`
+            `<td class="x-icon"><i class="fa-regular fa-pen-to-square"></i></td>`
           );
           $("#c-news").append(newsTr);
         } else {
@@ -76,7 +76,6 @@ var renderMembers = () => {
       userno: userno,
     },
     success: function (data) {
-      console.log(data); // 確認有撈到資料
       $(".username").text(data[1][0].username);
       $("#c-members").empty();
       $.each(data[0], function (i, list) {
@@ -84,10 +83,17 @@ var renderMembers = () => {
         newsTr.append(`<td>${list.userno}</td>`);
         newsTr.append(`<td>${list.id}</td>`);
         newsTr.append(`<td class="news-c"> ${list.password}</td>`);
-        newsTr.append(`<td>${list.nickname == null ? "" : list.nickname}</td>`);
+        if (list.permission == 0) {
+          newsTr.append(`<td>管理員</td>`);
+        } else {
+          newsTr.append(
+            `<td>${list.nickname == null ? "" : list.nickname}</td>`
+          );
+        }
         newsTr.append(`<td>${list.date}</td>`);
         $("#c-members").append(newsTr);
       });
+      $(".totalMember").text(`網站會員總數：${data[0].length}`);
     },
   });
 };
@@ -140,23 +146,67 @@ var postBtn = () => {
     : $(".c-change + .c-post").removeClass("postBtn");
 };
 /* ------------- (自動偵測必填欄位) ------------- */
-let oktoPost = true;
+let okPush = false;
+$("input,textarea").on("input", function () {
+  /* 顯示必填文字 */
+  let showMessage = $(this).next("span");
+  if (!$(this).val()) {
+    showMessage.html("*必填欄位");
+  } else {
+    showMessage.html("");
+  }
+});
+
+// // ----- POST 發布 -----
 $(".c-post").on("click", function () {
-  $("input,textarea").on("input", function () {
-    /* 顯示必填文字 */
-    let showMessage = $(this).next("span");
-    if (!$(this).val()) {
-      showMessage.html("*必填欄位");
-    } else {
-      showMessage.html("");
+  if (
+    !(
+      $(".c-textmessage").val() &&
+      $("input[type=date]").val() &&
+      $("textarea").val()
+    )
+  ) {
+    alert("請填寫必要內容");
+  } else {
+    let result = confirm("發布公告前記得確認文字都沒問題囉?!");
+    if (result) {
+      $.ajax({
+        url: url + "/admin/news/post",
+        type: "post",
+        data: {
+          newsno: $('input[name="newsno"]').val(),
+          title: $('input[type="text"]').val(),
+          content: $("textarea").val(),
+          release: $("input[type='date']").val(),
+        },
+        success: function (data) {
+          // console.log(data); // 確認有撈到資料
+          alert("公告發布成功");
+          $("input").val("");
+          $("textarea").val("");
+          postBtn();
+          renderNews();
+        },
+      });
     }
-  });
-  // ----- POST 發布 -----
-  let result = confirm("發布公告前記得確認文字都沒問題囉?!");
-  if (result) {
+  }
+});
+
+/* ------------- (按鍵更新) ------------- */
+// ----- UPDATE 更新 -----
+$(".c-change").on("click", function () {
+  if (
+    !(
+      $(".c-textmessage").val() &&
+      $("input[type=date]").val() &&
+      $("textarea").val()
+    )
+  ) {
+    alert("請填寫必要內容");
+  } else {
     $.ajax({
-      url: url + "/admin/news/post",
-      type: "post",
+      url: url + "/admin/news/update",
+      type: "PUT",
       data: {
         newsno: $('input[name="newsno"]').val(),
         title: $('input[type="text"]').val(),
@@ -165,7 +215,7 @@ $(".c-post").on("click", function () {
       },
       success: function (data) {
         // console.log(data); // 確認有撈到資料
-        alert("公告發布成功");
+        alert("公告更新成功");
         $("input").val("");
         $("textarea").val("");
         postBtn();
@@ -173,30 +223,6 @@ $(".c-post").on("click", function () {
       },
     });
   }
-});
-
-/* ------------- (按鍵更新) ------------- */
-// ----- UPDATE 更新 -----
-$(".c-change").on("click", function () {
-  console.log("OK");
-  $.ajax({
-    url: url + "/admin/members/update",
-    type: "PUT",
-    data: {
-      newsno: $('input[name="newsno"]').val(),
-      title: $('input[type="text"]').val(),
-      content: $("textarea").val(),
-      release: $("input[type='date']").val(),
-    },
-    success: function (data) {
-      // console.log(data); // 確認有撈到資料
-      alert("公告更新成功");
-      $("input").val("");
-      $("textarea").val("");
-      postBtn();
-      renderNews();
-    },
-  });
 });
 
 /* ------------- (按鍵刪除) ------------- */
@@ -210,9 +236,6 @@ $(".deleteBtn").on("click", function () {
     data: {
       newsno: $('input[name="newsno"]').val(),
       status: "F",
-      // title: $('input[type="text"]').val(),
-      // content: $("textarea").val(),
-      // release: $("input[type='date']").val(),
     },
     success: function (data) {
       // console.log(data); // 確認有撈到資料
@@ -229,24 +252,26 @@ $(".deleteBtn").on("click", function () {
 
 /* ------------- (按鍵更新) ------------- */
 $(".d-change").on("click", function () {
-  confirm("確定要變更會員資料嗎？");
-  $.ajax({
-    url: url + "admin/members/update",
-    type: "put",
-    data: {
-      userno: $('input[name="d-id"]').val(),
-      id: $('input[name="d-email"]').val(),
-      password: $('input[name="d-pwd"]').val(),
-    },
-    success: function (data) {
-      // console.log(data); // 確認有撈到資料
-      document.querySelector("#m-change").close();
-      alert("資料修改成功");
-      // document.querySelector("#d-okay").showModal();
-      // $("input").val("");
-      // $("textarea").val("");
+  var result = confirm("確定要變更會員資料嗎？");
+  if (result) {
+    $.ajax({
+      url: url + "admin/members/update",
+      type: "put",
+      data: {
+        userno: $('input[name="d-id"]').val(),
+        id: $('input[name="d-email"]').val(),
+        password: $('input[name="d-pwd"]').val(),
+      },
+      success: function (data) {
+        // console.log(data); // 確認有撈到資料
+        document.querySelector("#m-change").close();
+        alert("資料修改成功");
+        // document.querySelector("#d-okay").showModal();
+        // $("input").val("");
+        // $("textarea").val("");
 
-      renderMembers();
-    },
-  });
+        renderMembers();
+      },
+    });
+  }
 });
