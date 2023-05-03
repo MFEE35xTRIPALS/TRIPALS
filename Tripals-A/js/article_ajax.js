9ab95c34c3b5f5c1ac9c1a0b049a765a38fce098
Tina <t2239tina@gmail.com>
2023-05-03 16:23:11 +0800

    article value修正

--- Tripals-A/js/article_ajax.js
+++ Tripals-A/js/article_ajax.js
@@ -128,3 +128,53 @@ $("select").on("change", function () {
     },
   });
 });
+/* ----------------- 瀏覽數排序 ----------------- */
+$("select").on("change", function () {
+  // console.log($("select").val());
+  // var city = $("select").val();
+  $.ajax({
+    url: url + `/articles/views`,
+    type: "post",
+    data: { city: "台中市" },
+    success: function (data) {
+      console.log(data);
+      $(".c-mylikes").empty();
+      $.each(data, function (i, list) {
+        let card = `<div class="onecard card">
+        <a href="#" class="c-cardImg">
+          <div class="c-imgCover">
+            <p>more</p>
+          </div>
+          <img
+            class="Img"
+            src="./img/puppy-1207816_1280.jpg"
+            alt="文章首圖"
+            class="card-img-top"
+          />
+        </a>
+        <div class="card_body">
+          <div>
+            <a class="c-toSelfpage" href="#">
+              <h4 class="card-title">
+                ${list.title}
+              </h4>
+            </a>
+            <i class="heart fa-regular fa-heart"></i>
+          </div>
+          <a href="#" class="c-userName">
+            <h6>
+              <img src="./img/admin.png" alt="大頭照" class="head" />
+              ${list.username}
+            </h6>
+          </a>
+          <div class="viewsAndHeart">
+            <p><i class="fa-regular fa-heart"></i>0</p>
+            <p><i class="fa-regular fa-eye"></i> ${list.view_count}</p>
+          </div>
+        </div>
+      </div>`;
+        $(".c-mylikes").append(card);
+      });
+    },
+  });
+});
