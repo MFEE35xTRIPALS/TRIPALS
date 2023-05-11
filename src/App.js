bd014578ed40b8bbc25bd640a291691f6e99716e
henryx7878 <henryx7878@gmail.com>
2023-05-11 15:25:46 +0800

    0511-1

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
-import Header from './components/Header';
-import './components/CSS/Header.css'
-import './App.css';
+import React, { Component } from 'react';
+import Header from './components/Header.jsx';
+import 'bootstrap/dist/css/bootstrap.min.css'
+
 function App() {
   return (
     <div className="App">
