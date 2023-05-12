f3f06fdd7510cd03cbfdbf23bbe8a90508a02b8a
henryx7878 <henryx7878@gmail.com>
2023-05-12 09:41:59 +0800

    0512-2

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,51 +1,80 @@
-import React from "react";
+import React, { Component } from "react";
+import Logo from '../media/LOGO.svg';
 
 
-
-
-
-class Footer extends React.Component {
+class Footer extends Component {
+  state = {
+    url: "http://localhost:8000",
+    userno: 1,
+  };
   render() {
     return (
-      <footer>
-        <div className="container">
-          <div id="footerLogo">
-            <img src="../media/LOGO.svg" alt="" />
-            <h5>Copyright@Tripals All Rights Reserved.</h5>
+      <div>
+        {/* <!-- footer --> */}
+        <footer>
+          <div className="container">
+            <div id="footerLogo">
+              <img src={Logo} alt="" />
+              <h5>Copyright@Tripals All Rights Reserved.</h5>
+            </div>
+            <div id="footerNav">
+              <ul className="footer_nav nav">
+                <li>
+                  <a className="footer_en" href="#">
+                    TOP
+                  </a>
+                  <a className="footer_ch" href="#">
+                    首頁
+                  </a>
+                </li>
+                <li>
+                  <a className="footer_en" href="#">
+                    NEWS
+                  </a>
+                  <a className="footer_ch" href="#">
+                    最新消息
+                  </a>
+                </li>
+                <li className="nav-item">
+                  <a className="footer_en" href="#">
+                    DESTINATIONS
+                  </a>
+                  <a className="footer_ch" href="#">
+                    目的地
+                  </a>
+                </li>
+                <li className="nav-item">
+                  <a className="footer_en" href="#">
+                    GUIDE
+                  </a>
+                  <a className="footer_ch" href="#">
+                    旅遊導覽
+                  </a>
+                </li>
+                <li className="nav-item">
+                  <a className="footer_en" href="#">
+                    WRITE
+                  </a>
+                  <a className="footer_ch" href="#">
+                    寫文章
+                  </a>
+                </li>
+                <li className="nav-item">
+                  <a className="footer_en" href="mailto:info@example.com">
+                    CONTACT
+                  </a>
+                  <a className="footer_ch" href="mailto:info@example.com">
+                    聯絡我們
+                  </a>
+                </li>
+              </ul>
+            </div>
           </div>
-          <div id="footerNav">
-            <ul className="footer_nav nav">
-              <li>
-                <a className="footer_en" href="#">TOP</a>
-                <a className="footer_ch" href="#">首頁</a>
-              </li>
-              <li>
-                <a className="footer_en" href="#">NEWS</a>
-                <a className="footer_ch" href="#">最新消息</a>
-              </li>
-              <li className="nav-item">
-                <a className="footer_en" href="#">DESTINATIONS</a>
-                <a className="footer_ch" href="#">目的地</a>
-              </li>
-              <li className="nav-item">
-                <a className="footer_en" href="#">GUIDE</a>
-                <a className="footer_ch" href="#">旅遊導覽</a>
-              </li>
-              <li className="nav-item">
-                <a className="footer_en" href="#">WRITE</a>
-                <a className="footer_ch" href="#">寫文章</a>
-              </li>
-              <li className="nav-item">
-                <a className="footer_en" href="mailto:info@example.com">CONTACT</a>
-                <a className="footer_ch" href="mailto:info@example.com">聯絡我們</a>
-              </li>
-            </ul>
-          </div>
-        </div>
-      </footer>
-    )
+        </footer>
+      </div>
+    );
   }
+  async componentDidMount() { }
 }
 
-
-export default Footer
+export default Footer;
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -75,42 +75,42 @@ const Header = () => {
     };
   }, []);
 
-    return (
-        <header className="fixed-top">
-            <nav className="navbar navbar-expand-lg navbar-light">
-                <div className="container-fluid">
+  return (
+    <header className="fixed-top">
+      <nav className="navbar navbar-expand-lg navbar-light">
+        <div className="container-fluid">
 
-                    <button className="navbar-toggler border-0 hamburger hamburger--squeeze" type="button" data-bs-toggle="collapse"
-                        data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false"
-                        aria-label="Toggle navigation">
-                        <span className="hamburger-box">
-                            <span className="hamburger-inner"></span>
-                        </span>
-                    </button>
-                    <div className="collapse navbar-collapse" id="navbarSupportedContent">
-                        <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-                            <li className="nav-item">
-                                <a className="navWord" href="#" data-en="GUIDE" data-zh="旅遊導覽">GUIDE</a>
-                            </li>
-                            <li className="nav-item">
-                                <a className="navWord" href="#" data-en="WRITE" data-zh="寫文章">WRITE</a>
-                            </li>
-                            <li className="nav-item">
-                                <a className="navWord" href="#" data-en="DESTINATIONS" data-zh="目的地">DESTINATIONS</a>
-                            </li>
-                        </ul>
+          <button className="navbar-toggler border-0 hamburger hamburger--squeeze" type="button" data-bs-toggle="collapse"
+            data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false"
+            aria-label="Toggle navigation">
+            <span className="hamburger-box">
+              <span className="hamburger-inner"></span>
+            </span>
+          </button>
+          <div className="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+              <li className="nav-item">
+                <a className="navWord" href="#" data-en="GUIDE" data-zh="旅遊導覽">GUIDE</a>
+              </li>
+              <li className="nav-item">
+                <a className="navWord" href="#" data-en="WRITE" data-zh="寫文章">WRITE</a>
+              </li>
+              <li className="nav-item">
+                <a className="navWord" href="#" data-en="DESTINATIONS" data-zh="目的地">DESTINATIONS</a>
+              </li>
+            </ul>
 
 
-                        <div className="d-flex">
-                            <a className="navWord" id="house" href="#"><i className="fa-solid fa-house"></i></a>
-                            <button className="btn me-auto">SIGN UP</button>
-                            <button className="btn me-auto">LOG IN</button>
-                        </div>
-                    </div>
-                </div>
-            </nav>
-        </header>
-    )
+            <div className="d-flex">
+              <a className="navWord" id="house" href="#"><i className="fa-solid fa-house"></i></a>
+              <button className="btn me-auto">SIGN UP</button>
+              <button className="btn me-auto">LOG IN</button>
+            </div>
+          </div>
+        </div>
+      </nav>
+    </header>
+  )
 }
 
 
