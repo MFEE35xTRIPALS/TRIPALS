ec7a0427e9b0bd4f64f36021ac00b9c63ae97eea
6kk9k <124235286+6kk9k@users.noreply.github.com>
2023-05-14 22:53:29 +0800

    RWD地圖固定上方
    RWD地圖固定上方

--- src/components/ViewArticle/components/MapComponent.jsx
+++ src/components/ViewArticle/components/MapComponent.jsx
@@ -2,8 +2,7 @@ import React, { Component } from 'react';
 import { GoogleMap, LoadScript, MarkerF } from '@react-google-maps/api';
 
 const containerStyle = {
-  width: '100%',
-  height: '100vh',
+  height: '100%',
 };
 
 const center = {
