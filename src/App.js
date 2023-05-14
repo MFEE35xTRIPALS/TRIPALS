d41f779ae7d9829a23b27d95bc8dafb62f7da4d4
6kk9k <124235286+6kk9k@users.noreply.github.com>
2023-05-14 15:28:37 +0800

    vArticle
    vArticle

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+// import { BrowserRouter, Routes, Route} from 'react-router-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import ViewArticle from "./components/ViewArticle/ViewArticle";
+
+class App extends Component {
+  state = {  } 
+  render() { 
+    return (
+      <ViewArticle/>
+    );
+  }
 }
 
 export default App;
---
+++ src/components/ViewArticle/components/MapComponent.jsx
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import GoogleMapReact from 'google-map-react';
+
+class MapComponent extends Component {
+  static  = {
+    
+  };
+
+  render() {
+    return (
+      <h1>hello</h1>
+    );
+  }
+}
+
+export default MapComponent;
+
