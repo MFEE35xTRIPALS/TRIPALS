4a814167bd9b443580db8f8abee2ac405969bb51
henryx7878 <henryx7878@gmail.com>
2023-05-15 01:25:38 +0800

    0515

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import Header from './components/Header.jsx';
 import BearLogo from './components/BearLogo.jsx';
-import Home from './pages/Home/Home.jsx'
+import Home from './pages/Home/Home.jsx';
 import Footer from './components/Footer.js';
 import 'bootstrap/dist/css/bootstrap.min.css';
 function App() {
