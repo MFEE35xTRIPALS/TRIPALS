57b269839f1e727d31e1ed5549bbd57dc2a82f29
6kk9k <124235286+6kk9k@users.noreply.github.com>
2023-05-14 23:11:12 +0800

    改marker ICON
    改marker ICON

--- src/components/ViewArticle/components/MapComponent.jsx
+++ src/components/ViewArticle/components/MapComponent.jsx
@@ -26,7 +26,17 @@ class MapComponent extends Component {
           zoom={16}
           options={options}
         >
-           <MarkerF position={center} />
+           <MarkerF 
+           icon={{
+            path:
+              "M8 12l-4.7023 2.4721.898-5.236L.3916 5.5279l5.2574-.764L8 0l2.3511 4.764 5.2574.7639-3.8043 3.7082.898 5.236z",
+            fillColor: "yellow",
+            fillOpacity: 0.9,
+            scale: 2,
+            strokeColor: "gold",
+            strokeWeight: 2,
+          }}
+           position={center} />
         
         </GoogleMap>
       </LoadScript>
