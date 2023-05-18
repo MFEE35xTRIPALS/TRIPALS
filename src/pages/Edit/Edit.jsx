aced0270d0da983e2509f52db8643cb3104080e5
revolt125 <revolt125@gmail.com>
2023-05-19 02:35:33 +0800

    小修

--- src/pages/Edit/Edit.jsx
+++ src/pages/Edit/Edit.jsx
@@ -28,7 +28,7 @@ const Edit = ({ currentUser, setCurrentUser, avatarImg }) => {
 	// 	console.log(mainArticle);
 	// }, [mainArticle]);
 	//39 2
-	const articleID = 48;
+	const articleID = 2;
 
 	const fetchData = async () => {
 		try {
